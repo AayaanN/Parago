e778237a2e5269e064e85ec242392fa9cbdbc11b
ya5er <75911486+ya5er@users.noreply.github.com>
2021-01-16 10:53:46 -0500

    Update popup.js

--- final/popup/popup.js
+++ final/popup/popup.js
@@ -67,7 +67,18 @@ saveNote.onclick = function () {
 };
 
 startTimer.onclick = function () {
+  var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
+        display.textContent = minutes + ":" + seconds;
 
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 }
